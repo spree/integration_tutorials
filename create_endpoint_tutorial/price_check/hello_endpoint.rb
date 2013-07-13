8482579312f75a53eba0f8241c26a96074c446e8
Dana Jones <djones@larkfarm.com>
2013-07-13 09:34:49 -0500

    Refactored endpoint per Pablo's suggestions

--- create_endpoint_tutorial/price_check/hello_endpoint.rb
+++ create_endpoint_tutorial/price_check/hello_endpoint.rb
@@ -7,9 +7,7 @@ class HelloEndpoint < EndpointBase
   end
 
   post '/product_existence_check' do
-    get_product_names
-
-    if @product_names.include?(@message[:payload]['product']['name'])
+    if product_names.include?(passed_in_name)
       process_result 200, { 'message_id' => @message[:message_id], 'message' => 'notification:info' }
     else
       process_result 200, { 'message_id' => @message[:message_id], 'message' => 'notification:warn' }
@@ -17,12 +15,8 @@ class HelloEndpoint < EndpointBase
   end
 
   post '/query_price' do
-    passed_in_name = @message[:payload]['product']['name']
-    get_product_names
-
-    if @product_names.include?(passed_in_name)
-      ## Find the product whose name matches what we're passing.
-      product = JSON.parse(File.read("product_catalog.json"))['products'].find{ |x| x['name'] == passed_in_name }
+    ## Find the product whose name matches what we're passing.
+    if product = products.find { |product| product['name'] == passed_in_name }
       process_result 200, { 'message_id' => @message[:message_id], 'message' => 'product:in_stock',
         'payload' => { 'product' => { 'name' => product['name'], 'price' => product['price'] }}}
     else
@@ -31,7 +25,15 @@ class HelloEndpoint < EndpointBase
   end
 
 private
-  def get_product_names
-    @product_names = JSON.parse(File.read("product_catalog.json"))['products'].map{|p| p["name"]}
+  def product_names
+    @product_names ||= products.map { |product| product["name"] }
+  end
+
+  def products
+    @products ||= JSON.parse(File.read("product_catalog.json"))['products']
+  end
+
+  def passed_in_name
+    @passed_in_name ||= @message[:payload]['product']['name']
   end
 end
